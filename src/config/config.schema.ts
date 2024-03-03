0967c4f752749f90d15974075dc7c1e8abf5ab07
roshannihaal <roshan.nihaal18@gmail.com>
2024-03-03 11:30:05 +0530

    Connection to redis completed

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 export const ConfigSchema = z.object({
   PORT: z.string().trim(),
   NODE_ENV: z.enum(['development', 'production']),
+  REDIS_HOST: z.string().trim(),
+  REDIS_EXPOSE_PORT: z.string().trim(),
 })
 
 export type ConfigSchema = z.input<typeof ConfigSchema>
