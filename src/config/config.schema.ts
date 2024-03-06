83710ee50a735012e9c63cd37f7dd6483050a812
roshannihaal <roshan.nihaal18@gmail.com>
2024-03-06 21:43:26 +0530

    Modified ConfigSchema

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -1,28 +1,38 @@
 import { z } from 'zod'
 
-export const ConfigSchema = z.object({
-  PORT: z.string().trim(),
-  NODE_ENV: z.enum(['development', 'production']),
-  REDIS_HOST: z.string().trim(),
-  REDIS_EXPOSE_PORT: z.string().trim(),
-  OTP_LENGTH: z
-    .string()
-    .transform((value) => parseInt(value, 10))
-    .refine((value) => value > 0, {
-      message: 'OTP length must be greater than 0',
-    }),
-  INCLUDE_ALPHABETS: z
-    .enum(['true', 'false'])
-    .transform((value) => value === 'true'),
-  INCLUDE_NUMBERS: z
-    .enum(['true', 'false'])
-    .transform((value) => value === 'true'),
-  OTP_VALIDITY: z
-    .string()
-    .transform((value) => parseInt(value, 10))
-    .refine((value) => value > 0, {
-      message: 'OTP length must be greater than 0',
-    }),
-})
+export const ConfigSchema = z
+  .object({
+    PORT: z.string().trim(),
+    NODE_ENV: z.enum(['development', 'production']),
+    REDIS_HOST: z.string().trim(),
+    REDIS_EXPOSE_PORT: z.string().trim(),
+    OTP_LENGTH: z
+      .string()
+      .transform((value) => parseInt(value, 10))
+      .refine((value) => value > 0 && value <= 10, {
+        message: 'OTP length must be between 1 and 10',
+      }),
+    INCLUDE_ALPHABETS: z
+      .enum(['true', 'false'])
+      .transform((value) => value === 'true'),
+    INCLUDE_NUMBERS: z
+      .enum(['true', 'false'])
+      .transform((value) => value === 'true'),
+    OTP_VALIDITY: z
+      .string()
+      .transform((value) => parseInt(value, 10))
+      .refine((value) => value >= 300 && value <= 1800, {
+        message: 'OTP validity must be between 5 and 30 minutes',
+      }),
+  })
+  .refine(({ INCLUDE_ALPHABETS, INCLUDE_NUMBERS }) => {
+    if (INCLUDE_ALPHABETS === true || INCLUDE_NUMBERS === true) {
+      return true
+    } else {
+      throw new Error(
+        'At least one of INCLUDE_ALPHABETS or INCLUDE_NUMBERS must be true',
+      )
+    }
+  })
 
 export type ConfigSchema = z.input<typeof ConfigSchema>
